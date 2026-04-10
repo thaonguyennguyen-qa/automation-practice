f7d4c912adc31075b8aa9ce1d317aa98ebb48433
nguyen.nguyen <thaonguyen.nguyen286@gmail.com>
2026-04-10 22:40:36 +0700

    Initial commit: add README and hello script

---
+++ test.js
@@ -0,0 +1,8 @@
+const name = "Automation Tester";
+const week = 1;
+
+console.log("============================");
+console.log("Xin chao! Toi la " + name);
+console.log("Day la tuan " + week + " cua khoa hoc");
+console.log("============================");
+console.log("Node version:", process.version);
